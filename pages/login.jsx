8b89f9ff32c6e6244e51a071e625a7c1de5386f5
Felix-9999 <105433860+Felix-9999@users.noreply.github.com>
2023-06-22 13:34:13 +0400

    push

--- pages/login.jsx
+++ pages/login.jsx
@@ -44,12 +44,12 @@ export default function Login() {
 
     // Google Handler function
     async function handleGoogleSignin() {
-        signIn('google', { callbackUrl: "http://localhost:3000" })
+        signIn('google', { callbackUrl: "https://crud-rosy-two.vercel.app" })
     }
 
     // Github Login 
     async function handleGithubSignin() {
-        signIn('github', { callbackUrl: "http://localhost:3000" })
+        signIn('github', { callbackUrl: "https://crud-rosy-two.vercel.app" })
     }
 
     return (
